ddd20362076266744c38c4c7d946987a1fabfec6
Porameht <porameht@outlook.com>
2022-05-06 17:42:12 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Box1 from './components/Box1'
+import Box2 from './components/Box2'
+import Box3 from './components/Box3'
+import { useSelector, useDispatch } from 'react-redux'
+import * as appActions from "./actions/app.action";
+export default function App() {
 
-function App() {
+  // const [count, setCount] = React.useState(0)
+  const appReducer =useSelector(({appReducer})=>appReducer)
+  const box1Reducer =useSelector(({box1Reducer})=>box1Reducer)
+  const dispatch = useDispatch();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{padding: 10}}>
+      <div style={{display:'flex', flexDirection:'row'}}>
+        <button onClick={()=>dispatch(appActions.add())}>ADD</button>
+        <button onClick={()=>dispatch(appActions.remove())}>REM</button>
+        <button onClick={()=>dispatch(appActions.clear(-1))}>CLR</button>
+      </div>
+      <h1>Frank Mechanical Engineer : {appReducer.count}, B1({box1Reducer.count1})</h1>
+      <Box1/>
+      <Box2/>
+      <Box3/>
     </div>
-  );
+  )
 }
-
-export default App;
